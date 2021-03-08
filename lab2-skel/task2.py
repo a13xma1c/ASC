f7343abda797443089f2e004d25b42a9f35cc268
a13xma1c <alexmcn07@gmail.com>
2021-03-08 08:41:51 +0200

    task 2 completed

--- lab2-skel/task2.py
+++ lab2-skel/task2.py
@@ -7,3 +7,27 @@
     e.g. Hello, I'm Thread-96 and I received the number 42
 
 """
+
+from threading import Thread
+import sys
+import random
+
+
+def thread_function(thread_id, nr):
+    print("Hello, I'm Thread-%s and I received the number %s" % (thread_id, nr))
+
+
+MAX_NUM = 100000
+random.seed()
+nr_threads = int(sys.argv[1])
+thread_list = []
+
+for i in range(0, nr_threads):
+    t = Thread(target=thread_function, args=(i, random.randint(0, MAX_NUM)))
+    thread_list.append(t)
+
+for t in thread_list:
+    t.start()
+
+for t in thread_list:
+    t.join()
